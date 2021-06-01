079f3c8f250755e5e4b020a6d5a793f1edf18996
Parthesh Patel <parthesh152@gmail.com>
2021-05-31 21:35:47 -0400

    player class

--- poker.py
+++ poker.py
@@ -1,17 +1,34 @@
 import poker
 
-def dealCards(deck):
-    return 0
-def run():
-    #get number of players
-    numPlayers = input("Number of players:")
-    #play game
-    play(numPlayers)
+class Player:
+    chipcount = 0
+    cards = []
+    def __init__(self, startingChipCount):
+        self.chipcount = startingChipCount
+    def setHand(self, card1, card2):
+        self.cards = [card1, card2]
+    def resetHand(self):
+        self.cards = []
 
-def play(numPlayer):
+def setblinds(first, numPlayer,bigB,smallB,dealer):
+    if numPlayer > 2:
+        smallB = 0
+        bigB = 1
+
+
+def play(numPlayer, startingChipCount):
     continueGame = True
+    players = []
+    for x in range(numPlayer):
+        tempPlayer = Player(startingChipCount)
+        players.append(tempPlayer)
+    #set which players are big,small blind and dealer
+    bigb,smallB,dealer
+    first = True
+    setblinds(first, numPlayer,bigB,smallB,dealer)
     #while game is being played
     while continueGame:
+        first = False
         #take blind bets
         blindBets()
         #deal 2 cards to each player
@@ -28,8 +45,14 @@ def play(numPlayer):
         #showdown
         showdown()
         #deal another hand?
-        checkGame()
+        continueGame = checkGame()
 
+def run():
+    #get number of players
+    numPlayers = int(input("Number of players:"))
+    startingChipCount = int(input("Buy in:"))
+    #play game
+    play(numPlayers, startingChipCount)
 if __name__ == "__main__":
     run()
 
